caf859d46d0b2a74adbaa6a33a19ac7d8d9daff2
tiago.ameller@gmail.com <tiago.ameller@gmail.com>
2016-07-17 10:44:45 +0200

    make rubocop happy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
-    !!current_user
+    current_user.present?
   end
 
   protected
